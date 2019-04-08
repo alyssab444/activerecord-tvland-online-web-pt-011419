a52ff9f4bc0502b3f1faa1e97610b0eb70e796ad
Alyssa Brown <alyssa.brown@live.com>
2019-04-08 01:26:42 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_column_actor_id_to_actors.rb
+++ db/migrate/006_add_column_actor_id_to_actors.rb
@@ -1,4 +1,4 @@
-class AddColumnActorIdToCharacters < ActiveRecord::Migration
+class AddColumnActorIdToCharacters < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :actor_id, :integer
   end
